bc1d9d8bbce65121e628bdf80dea56bdf57f2cc1
emmanuel <emmanuelukwuegbu2016@gmail.com>
2018-09-03 03:17:56 -0700

    added settings for static root

--- easywriter/settings/base.py
+++ easywriter/settings/base.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
+    'captcha',
     'accounts',
     'blog'
 
@@ -49,7 +50,7 @@ ROOT_URLCONF = 'easywriter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
